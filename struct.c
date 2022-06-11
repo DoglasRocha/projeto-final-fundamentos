6e220401bf57fac0329fc955d0ae529a5cbd1a07
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-06-10 22:00:49 -0300

    refatorar funcao 'calcula_vivo_morto'

--- struct.c
+++ struct.c
@@ -20,7 +20,6 @@ typedef struct{
     int dim1,dim2; //dimensoes do tabuleiro linhas x colunas
     char **m; //Atenção! Essa matriz terá que ser alocada dinamicamente
     //para que a funcao que inicializa possa funcionar
-    char **mAnterior;
 }Tab;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -37,11 +36,11 @@ void inicGlider(Tab *tabuleiro);
 void inicLWSS(Tab *tabuleiro);
 void menuInicJogo(Tab *tabuleiro);
 void imprimeMatriz(Tab *tabuleiro);
-void copiaMatriz(Tab *tabuleiro);
+void copiaMatriz(Tab *tabuleiro, char **mAnterior);
 void jogaJogoVida(Tab *tabuleiro);
-void atualizaMat(Tab *tabuleiro);
+void atualizaMat(Tab *tabuleiro, char **mAnterior);
 void vizinhos_possiveis(int *tamanho, int pos_atual,  Tab *tabuleiro, int vizinhos[]);
-char calcula_vivo_morto(int pos_x_possiveis[], int pos_y_possiveis[], int tam_x, int tam_y, Tab *tabuleiro, int x_atual, int y_atual);
+char calcula_vivo_morto(int pos_possiveis[2][3], int tamanhos[2], char **mAnterior, int pos_atual[2]);
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -61,8 +60,6 @@ int main()
         printf("Digite o numero de linhas da matriz, o numero  de colunas da matriz, e o numero de ciclos, separados por espaços (valores inteiros): ");
 
         scanf(" %d %d %d", &tabuleiro.dim1, &tabuleiro.dim2, &tabuleiro.ciclosVida);
-
-    
 
         tabuleiro.m = alocaMatriz(&tabuleiro);
 
@@ -170,7 +167,7 @@ void menuInicJogo(Tab *tabuleiro)
 
     imprimeMatriz(tabuleiro);
 
-    printf("Se inicializacao correta digite ENTER tecla para iniciar o jogo..."); 
+    printf("Se inicializacao correta, digite a tecla ENTER para iniciar o jogo..."); 
     while(getchar()!='\n')
     ; 
     getchar();
@@ -188,18 +185,23 @@ void jogaJogoVida(Tab *tabuleiro)
     // getchar();
     sleep(TMP_ESPERA);
 
-    tabuleiro->mAnterior = alocaMatriz(tabuleiro);
+    mAnt = alocaMatriz(tabuleiro);
 
     for(c=1;c<=tabuleiro->ciclosVida;c++)
     {
-        copiaMatriz(tabuleiro);
+        copiaMatriz(tabuleiro, mAnt);
 
-        atualizaMat(tabuleiro);//atualizaMat(mAtual,mAnt,nL,nC);
+        atualizaMat(tabuleiro, mAnt);//atualizaMat(mAtual,mAnt,nL,nC);
         system(LIMPA);
         imprimeMatriz(tabuleiro);
         
         sleep(TMP_ESPERA);
     }
+
+    for (int i = 0; i < tabuleiro->dim1; i++)
+        free(mAnt[i]);
+
+    free(mAnt);
 }
 
 char **alocaMatriz(Tab *tabuleiro)
@@ -219,10 +221,6 @@ void desalocaMatriz(Tab *tabuleiro)
         free(tabuleiro->m[i]);
 
     free(tabuleiro->m);
-    for (int i = 0; i < tabuleiro->dim1; i++)
-        free(tabuleiro->mAnterior[i]);
-
-    free(tabuleiro->mAnterior);
 }
 
 void imprimeMatriz(Tab *tabuleiro)
@@ -236,24 +234,24 @@ void imprimeMatriz(Tab *tabuleiro)
     }
 }
 
-void copiaMatriz(Tab *tabuleiro)
+void copiaMatriz(Tab *tabuleiro, char **mAnterior)
 {
     int l, c;
     for(l = 0; l < tabuleiro->dim1; l++)
-        for(c = 0; c < tabuleiro->dim2; c++) tabuleiro->mAnterior[l][c] = tabuleiro->m[l][c];
+        for(c = 0; c < tabuleiro->dim2; c++) mAnterior[l][c] = tabuleiro->m[l][c];
 }
-void atualizaMat(Tab *tabuleiro)
+void atualizaMat(Tab *tabuleiro, char **mAnterior)
 {
-    int pos_x_possiveis[3], pos_y_possiveis[3], tam_x, tam_y;
+    int pos_possiveis[2][3], tamanhos[2], pos_atual[2];
 
     for (int i = 0; i < tabuleiro->dim1; i++)
     {
-        vizinhos_possiveis(&tam_x, i,tabuleiro, pos_x_possiveis);
+        vizinhos_possiveis(&tamanhos[0], i,tabuleiro, pos_possiveis[0]);
         for (int j = 0; j < tabuleiro->dim2; j++)
         {
-            vizinhos_possiveis(&tam_y, j, tabuleiro, pos_y_possiveis);
-
-            tabuleiro->m[i][j] = calcula_vivo_morto(pos_x_possiveis, pos_y_possiveis, tam_x, tam_y, tabuleiro, i, j);
+            vizinhos_possiveis(&tamanhos[1], j, tabuleiro, pos_possiveis[1]);
+            pos_atual[0] = i, pos_atual[1] = j;
+            tabuleiro->m[i][j] = calcula_vivo_morto(pos_possiveis, tamanhos, mAnterior, pos_atual);
         }
     }
 }
@@ -279,7 +277,7 @@ void vizinhos_possiveis(int *tamanho, int pos_atual,  Tab *tabuleiro, int vizinh
         *tamanho = 3;
 }
 
-char calcula_vivo_morto(int pos_x_possiveis[], int pos_y_possiveis[], int tam_x, int tam_y, Tab *tabuleiro, int x_atual, int y_atual)
+char calcula_vivo_morto(int pos_possiveis[2][3], int tamanhos[2], char **mAnterior, int pos_atual[2])
 {
     /* Uma célula viva morre de solidão se tiver menos de duas vizinhas vivas.
     Uma célula viva morre por superpopulação se tiver mais que três vizinhas vivas.
@@ -287,14 +285,14 @@ char calcula_vivo_morto(int pos_x_possiveis[], int pos_y_possiveis[], int tam_x,
     Uma célula morta ganha vida se tiver exatamente três vizinhas vivas */
     int vivos = 0;
 
-    for (int i = 0; i < tam_x; i++)
-        for (int j = 0; j < tam_y; j++)
+    for (int i = 0; i < tamanhos[0]; i++)
+        for (int j = 0; j < tamanhos[1]; j++)
         {
-            if (pos_x_possiveis[i] == x_atual && pos_y_possiveis[j] == y_atual) continue;
-            if (tabuleiro->mAnterior[pos_x_possiveis[i]][pos_y_possiveis[j]] == 'X') vivos++;
+            if (pos_possiveis[0][i] == pos_atual[0] && pos_possiveis[1][j] == pos_atual[1]) continue;
+            if (mAnterior[pos_possiveis[0][i]][pos_possiveis[1][j]] == 'X') vivos++;
         }
 
-    if (tabuleiro->mAnterior[x_atual][y_atual] == 'X')
+    if (mAnterior[pos_atual[0]][pos_atual[1]] == 'X')
         return (vivos > 3 || vivos < 2) ? '.' : 'X';
 
     return vivos == 3 ? 'X' : '.';
